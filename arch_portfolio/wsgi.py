1c4fd50ef1d2debc3ef744a8df9c16ce22cfc390
Aditya-aot <aditya.aot.11@gmail.com>
2024-03-08 17:08:04 +0530

    logo updat

--- arch_portfolio/wsgi.py
+++ arch_portfolio/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arch_portfolio.settings')
 
 application = get_wsgi_application()
+
+# vercel 
+app=application
+# vercel
