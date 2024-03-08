6e7dc95835f4cf8a2456c15ef83e197e6490c7a6
Aditya-aot <aditya.aot.11@gmail.com>
2024-03-08 19:50:01 +0530

    new 7 change

--- arch_portfolio/settings.py
+++ arch_portfolio/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # vercel
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # vercel
 
 # python anywhere
